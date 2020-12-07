7d544f8c8d0a192682c40001c030b7b1c2c60dcc
smaranika05 <smaranikadatta05@gmail.com>
2020-12-07 19:30:52 +0530

    backend5

--- Backend/index.js
+++ Backend/index.js
@@ -9,37 +9,37 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
-
+​
 const oauth2Client = new OAuth2(
   "334223947302-fejcs001663k3hg2tdrqmqdd85711ma8.apps.googleusercontent.com", // ClientID
   "LL8jGKLtf4iSmLQPxdPhriuh", // Client Secret
   "https://developers.google.com/oauthplayground" // Redirect URL
 );
-
+​
 oauth2Client.setCredentials({
   refresh_token:
     "1//04GpvZGrYsN1MCgYIARAAGAQSNwF-L9IrtXKXbI9eqOMg3yDkCKXtI_yVv-GwhZib7sESoMbTlzvSdORx_Ffkr6bGVb5ZvroXel0",
 });
 const accessToken = oauth2Client.getAccessToken();
-
+​
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
   auth: {
     type: "OAuth2",
-    user: "praxiswebsite.de@gmail.com",
+    // user: "praxiswebsite.de@gmail.com",
     clientId:
       "334223947302-fejcs001663k3hg2tdrqmqdd85711ma8.apps.googleusercontent.com",
     clientSecret: "LL8jGKLtf4iSmLQPxdPhriuh",
-    refreshToken:
-      "1//04GpvZGrYsN1MCgYIARAAGAQSNwF-L9IrtXKXbI9eqOMg3yDkCKXtI_yVv-GwhZib7sESoMbTlzvSdORx_Ffkr6bGVb5ZvroXel0",
-    accessToken: accessToken,
+    // refreshToken:
+    //   "1//04GpvZGrYsN1MCgYIARAAGAQSNwF-L9IrtXKXbI9eqOMg3yDkCKXtI_yVv-GwhZib7sESoMbTlzvSdORx_Ffkr6bGVb5ZvroXel0",
+    // accessToken: accessToken,
   },
 });
-
+​
 tls: {
   rejectUnauthorized: false;
 }
-
+​
 contactEmail.verify((error) => {
   if (error) {
     console.log(error);
@@ -47,7 +47,7 @@ contactEmail.verify((error) => {
     console.log("Ready to Send");
   }
 });
-
+​
 router.post("/contact", (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
@@ -58,6 +58,13 @@ router.post("/contact", (req, res) => {
   const slide2 = req.body.slide2;
   const slide3 = req.body.slide3;
   const mail = {
+    auth: {
+      user: "praxiswebsite.de@gmail.com",
+      refreshToken:
+        "1//04GpvZGrYsN1MCgYIARAAGAQSNwF-L9IrtXKXbI9eqOMg3yDkCKXtI_yVv-GwhZib7sESoMbTlzvSdORx_Ffkr6bGVb5ZvroXel0",
+      accessToken: accessToken,
+      expires: 1484314697598,
+    },
     from: name,
     to: "shishirahere@gmail.com",
     subject: "Email from Praxis Website",
@@ -71,18 +78,18 @@ router.post("/contact", (req, res) => {
     }
   });
 });
-
+​
 // const express = require("express");
 // const router = express.Router();
 // const cors = require("cors");
 // const nodemailer = require("nodemailer");
-
+​
 // const app = express();
 // app.use(cors());
 // app.use(express.json());
 // app.use("/", router);
 // app.listen(5000, () => console.log("Server Running"));
-
+​
 // const contactEmail = nodemailer.createTransport({
 //   host: "mail.softsourced.de",
 //   port: 465,
@@ -97,7 +104,7 @@ router.post("/contact", (req, res) => {
 //     ciphers: "SSLv3",
 //   },
 // });
-
+​
 // contactEmail.verify((error) => {
 //   if (error) {
 //     console.log(error);
@@ -105,7 +112,7 @@ router.post("/contact", (req, res) => {
 //     console.log("Ready to Send");
 //   }
 // });
-
+​
 // router.post("/contact", (req, res) => {
 //   const name = req.body.name;
 //   const email = req.body.email;
