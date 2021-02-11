222ab1867b54c8bacee9223c5acc466e157b7adb
smaranika05 <smaranikadatta05@gmail.com>
2021-02-11 19:41:44 +0530

    cookie

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Suspense, lazy } from "react";
-
+​
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+​
 import { Helmet } from "react-helmet";
 import "./App.css";
 import Loader from "./Components/Loader/index";
 import Card from "./Views/Pakete/Package_contact/Package_contact";
-
+import CookieConsent from "react-cookie-consent";
 const BlogPage = lazy(() => import("./Views/Blog_pages/blog_page"));
 const Kont = lazy(() => import("./Views/Kontakt/kontakt"));
 const Refer = lazy(() => import("./Views/Refer/refer"));
@@ -17,11 +17,9 @@ const Pakt = lazy(() => import("./Views/Pakete/pakete"));
 const Footer = lazy(() => import("./Components/footer/footer"));
 const Impressum = lazy(() => import("./Views/Impressum/impressum"));
 const PrivacyPolicy = lazy(() => import("./Views/PrivacyPolicy/pp"));
-
+​
 function App() {
   return (
-    
-   
     <Suspense fallback={<Loader />}>
       <Router>
         <Helmet>
@@ -41,9 +39,25 @@ function App() {
         <Route exact path="/datenschutzerklaerung" component={PrivacyPolicy} />
         <Route exact path="/blog/:blogid" component={BlogPage} />
         <Route exact path="/Pakete#card_section" component={Card} />
-       
-
+​
         <Footer />
+        <CookieConsent
+          buttonText="Akzeptieren"
+          declineButtonText="Decline"
+          style={{ background: "black" }}
+          buttonStyle={{
+            backgroundColor: "#204379",
+            color: "white",
+            border: "1px solid white",
+          }}
+          enableDeclineButton
+          onDecline={() => {
+            alert("Declined!");
+          }}
+        >
+          Diese Website verwendet Cookies, um die Benutzererfahrung zu
+          verbessern.
+        </CookieConsent>
       </Router>
     </Suspense>
   );
